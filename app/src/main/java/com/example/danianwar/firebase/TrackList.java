688d78069638e187ddcc94535006fde6edb7b7f0
indragunawan222 <ig225970@gmail.com>
2019-09-23 13:18:51 +0700

    tugas

---
+++ app/src/main/java/com/example/danianwar/firebase/TrackList.java
@@ -0,0 +1,41 @@
+package com.example.danianwar.firebase;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class TrackList extends ArrayAdapter<Track> {
+    private Activity context;
+    private List<Track> trackList;
+
+    public TrackList(Activity context, List<Track> trackList) {
+        super(context, R.layout.layout_track_list, trackList);
+        this.context = context;
+        this.trackList = trackList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.layout_track_list, null, true);
+
+        TextView tvName = (TextView) listViewItem.findViewById(R.id.tvName);
+        TextView tvRating = (TextView) listViewItem.findViewById(R.id.tvRating);
+
+        Track track = trackList.get(position);
+
+        tvName.setText(track.getTrackName());
+        tvRating.setText(track.getTrackRating()+"");
+
+        return listViewItem;
+    }
+}
